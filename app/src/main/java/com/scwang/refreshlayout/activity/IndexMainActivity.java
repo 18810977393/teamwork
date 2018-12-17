272239832ef7e6a9faa7b035f6bb83235ca9a21a
18811391633 <2495474440@qq.com>
2018-12-17 19:24:14 +0800

    完成个人中心昵称，个性签名的修改功能

--- app/src/main/java/com/scwang/refreshlayout/activity/IndexMainActivity.java
+++ app/src/main/java/com/scwang/refreshlayout/activity/IndexMainActivity.java
@@ -1,5 +1,7 @@
 package com.scwang.refreshlayout.activity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.annotation.IdRes;
@@ -12,6 +14,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 import com.avos.avoscloud.AVAnalytics;
@@ -21,6 +25,8 @@ import com.avos.avoscloud.AVQuery;
 import com.avos.avoscloud.AVUser;
 import com.avos.avoscloud.FindCallback;
 import com.scwang.refreshlayout.R;
+import com.scwang.refreshlayout.activity.Mine.LoginActivity;
+import com.scwang.refreshlayout.activity.Task.ProfilePracticeActivity;
 import com.scwang.refreshlayout.fragment.index.MineFragment;
 import com.scwang.refreshlayout.fragment.index.TaskFragment;
 import com.scwang.refreshlayout.fragment.index.AwardFragment;
@@ -45,7 +51,7 @@ public class IndexMainActivity extends AppCompatActivity implements OnNavigation
         private final int menuId;
         private final Class<? extends Fragment> clazz;
 
-        TabFragment(@IdRes int menuId, Class<? extends Fragment> clazz) {
+        TabFragment(@IdRes int menuId, Class<? extends  Fragment> clazz) {
             this.menuId = menuId;
             this.clazz = clazz;
         }
--- app/src/main/java/com/scwang/refreshlayout/activity/Task/ProfilePracticeActivity.java
+++ app/src/main/java/com/scwang/refreshlayout/activity/Task/ProfilePracticeActivity.java
@@ -1,18 +1,25 @@
 package com.scwang.refreshlayout.activity.Task;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.avos.avoscloud.AVUser;
 import com.scwang.refreshlayout.R;
 import com.scwang.refreshlayout.activity.Mine.LoginActivity;
 import com.scwang.refreshlayout.activity.Mine.RegisterActivity;
 import com.scwang.refreshlayout.util.StatusBarUtil;
+
+import org.w3c.dom.Text;
 
 /**
  * 个人中心
@@ -26,6 +33,27 @@ public class ProfilePracticeActivity extends Activity {
         setContentView(R.layout.activity_practice_profile);
 
         final Toolbar toolbar = findViewById(R.id.toolbar);
+
+        final TextView textView = (TextView)findViewById(R.id.user_names);
+
+        final Button button = (Button) findViewById(R.id.fix_name);
+        final Button button2 = (Button) findViewById(R.id.QianMing);
+
+        button.setText(AVUser.getCurrentUser().getUsername());
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog();
+
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog2();
+            }
+        });
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,6 +76,59 @@ public class ProfilePracticeActivity extends Activity {
                 ProfilePracticeActivity.this.finish();
             }
         });
+    }
+
+    public void alertDialog(){
+        View view = getLayoutInflater().inflate(R.layout.half_dialog_view, null);
+        final EditText editText = (EditText) view.findViewById(R.id.usernames);
+        final TextView textView = (TextView)findViewById(R.id.user_names);
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setIcon(R.mipmap.heart)//设置标题的图片
+                .setTitle("修改内容")//设置对话框的标题
+                .setView(view)
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String content = editText.getText().toString();
+                        Toast.makeText(ProfilePracticeActivity.this, "修改成功", Toast.LENGTH_SHORT).show();
+                        textView.setText(content);
+                        dialog.dismiss();
+                    }
+                }).create();
+        dialog.show();
+
+    }
+    public void alertDialog2(){
+        View view = getLayoutInflater().inflate(R.layout.half_dialog_view, null);
+        final EditText editText = (EditText) view.findViewById(R.id.usernames);
+        final TextView textView2 = (TextView)findViewById(R.id.SignName);
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setIcon(R.mipmap.heart)//设置标题的图片
+                .setTitle("修改内容")//设置对话框的标题
+                .setView(view)
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String content = editText.getText().toString();
+                        Toast.makeText(ProfilePracticeActivity.this, "修改成功", Toast.LENGTH_SHORT).show();
+                        textView2.setText(content);
+                        dialog.dismiss();
+                    }
+                }).create();
+        dialog.show();
+
     }
 
 }
